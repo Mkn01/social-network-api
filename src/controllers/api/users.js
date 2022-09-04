4f02644355752b06a2c7dd1edc86057956d99c02
gurmanpreet nagra <gurmanpreetnagra@gmail.com>
2022-09-04 21:42:38 +0100

    fixed update user and delete user fn

--- src/controllers/api/users.js
+++ src/controllers/api/users.js
@@ -46,25 +46,58 @@ const createNewUser = async (req, res) => {
 
 const updateUserById = async (req, res) => {
   try {
-    const { id } = req.params;
     const { username, email } = req.body;
-    if (username || email) {
-      await User.findByIdAndUpdate(id, {
-        username,
-        email,
-      });
-      return res.json({ success: true });
+    const { id } = req.params;
+
+    const updatedUser = await User.findByIdAndUpdate(
+      id,
+      { username, email },
+      { new: true }
+    );
+
+    if (!updatedUser) {
+      console.log(`[ERROR]: Failed to find user | No user with id of ${id}`);
+
+      return res.status(404).json({ error: "Failed to find user" });
     }
+
+    return res.json({
+      success: true,
+      data: updatedUser,
+    });
   } catch (error) {
-    console.log(`[ERROR]: Failed to update user | ${error.message}`);
+    console.log(`[ERROR: Failed to update user | ${error.message}]`);
+
+    return res.status(500).json({
+      success: false,
+      error: "Failed to update user",
+    });
   }
 };
 
-const deleteUserById = (req, res) => {
+const deleteUserById = async (req, res) => {
   try {
-    return res.send("deleteUserById");
+    const { id } = req.params;
+
+    const user = await User.findByIdAndRemove(id);
+
+    if (!user) {
+      console.log(`[ERROR]: Failed to find user | No user with id of ${id}`);
+
+      return res.status(404).json({ error: "Failed to find user" });
+    }
+
+    return res.json({
+      success: true,
+      message: "Successfully deleted user",
+    });
   } catch (error) {
-    console.log(`[ERROR]: Failed to delete user | ${error.message}`);
+    console.log(`[ERROR: Failed to delete user | ${error.message}]`);
+
+    return res.status(500).json({
+      success: false,
+      error: "Failed to delete user",
+    });
   }
 };
 
